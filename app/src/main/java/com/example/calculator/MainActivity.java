0d4122a5f8990002aedf6c471fc4300fa1c0538d
Usman8902 <bsef20a545@pucit.edu.pk>
2023-07-08 05:33:35 -0700

    Calculator having four functions

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void Add(View v)
+    {
+        EditText edt1=(EditText) findViewById(R.id.editTextNumber);
+        EditText edt2=(EditText) findViewById(R.id.editTextNumber2);
+        EditText edt3=(EditText) findViewById(R.id.editTextNumber3);
+        int n1=Integer.parseInt(edt1.getText().toString());
+        int n2=Integer.parseInt(edt2.getText().toString());
+        int results=n1+n2;
+        edt3.setText("Sum Value=" + results);
+    }
+
 }
